2a111e0db30ac9d5aba7690c753df4172cd999e5
ArpitaKarChoudhury <106442501+ArpitaKarChoudhury@users.noreply.github.com>
2022-06-26 11:46:01 +0530

    Add files via upload

---
+++ sum of multiply.py
@@ -0,0 +1,13 @@
+# Write a function that returns the sum of multiples of 3 and 5 between 0 and 
+# limit (parameter). For example, if limit is 20, it should return the sum of 
+# 3, 5, 6, 9, 10, 12, 15, 18,20.
+def multi():
+    a=0
+    while a<=b:
+        if a%3==0:
+            print(a,end=",")
+        elif a%5==0:
+            print(a,end=",")
+        a=a+1
+b=int(input("enter number"))
+multi()
